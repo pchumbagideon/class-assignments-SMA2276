a1b13f7bc6ea880b282862d80972c4fcb6373b9c
Gideon Pchumba ENE211-0043/2019 <124683388+pchumbagideon@users.noreply.github.com>
2023-03-29 08:25:22 +0300

    Add files via upload

---
+++ cat 2/Question 3 correct.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+class Voter {
+private:
+    std::string voter_card_id;
+    std::string national_id;
+    std::string first_name;
+    std::string middle_name;
+    std::string surname;
+    std::string polling_station;
+    std::string date_of_birth;
+    std::string gender;
+
+public:
+    // Constructor
+    Voter(std::string voter_card_id, std::string national_id, std::string first_name,
+          std::string middle_name, std::string surname, std::string polling_station,
+          std::string date_of_birth, std::string gender)
+    {
+        this->voter_card_id = voter_card_id;
+        this->national_id = national_id;
+        this->first_name = first_name;
+        this->middle_name = middle_name;
+        this->surname = surname;
+        this->polling_station = polling_station;
+        this->date_of_birth = date_of_birth;
+        this->gender = gender;
+    }
+
+    // Default constructor, prompts for input
+    Voter()
+    {
+        std::cout << "Enter voter details:" << std::endl;
+        std::cout << "Voter Card ID: ";
+        std::getline(std::cin, voter_card_id);
+        std::cout << "National ID: ";
+        std::getline(std::cin, national_id);
+        std::cout << "First Name: ";
+        std::getline(std::cin, first_name);
+        std::cout << "Middle Name: ";
+        std::getline(std::cin, middle_name);
+        std::cout << "Surname: ";
+        std::getline(std::cin, surname);
+        std::cout << "Polling Station: ";
+        std::getline(std::cin, polling_station);
+        std::cout << "Date of Birth (dd-mm-yyyy): ";
+        std::getline(std::cin, date_of_birth);
+        std::cout << "Gender: ";
+        std::getline(std::cin, gender);
+    }
+
+    // Display voter details
+    void display_info() const {
+        std::cout << "Voter Details" << std::endl;
+        std::cout << "Voter Card ID: " << voter_card_id << std::endl;
+        std::cout << "National ID: " << national_id << std::endl;
+        std::cout << "Name: " << first_name << " " << middle_name << " " << surname << std::endl;
+        std::cout << "Polling Station: " << polling_station << std::endl;
+        std::cout << "Date of Birth: " << date_of_birth << std::endl;
+        std::cout << "Gender: " << gender << std::endl;
+    }
+};
+
+int main() {
+    char add_another_voter = 'y';
+    
+    while (tolower(add_another_voter) == 'y') {
+        // Create a new voter object
+        Voter my_voter;
+
+        // Display voter details
+        my_voter.display_info();
+        
+        std::cout << "Add another voter? (y/n): ";
+        std::cin >> add_another_voter;
+        std::cin.ignore(); // Ignore newline character in input buffer
+    }
+
+    return 0;
+}
---
+++ cat 2/Question 5 upd.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string> 
+ using namespace std; 
+  
+ class Employee { 
+ private: 
+     int employee_id; 
+     string first_name; 
+     string second_name; 
+     string surname; 
+     char gender; 
+     string date_of_birth; 
+     double monthly_basic_salary; 
+ public: 
+     Employee(int id, string fn, string sn, string snn, char g, string dob, double mbs) { 
+         employee_id = id; 
+         first_name = fn; 
+         second_name = sn; 
+         surname = snn; 
+         gender = g; 
+         date_of_birth = dob; 
+         monthly_basic_salary = mbs; 
+     } 
+     void show_employee() { 
+         cout << "Employee ID: " << employee_id << endl; 
+         cout << "Name: " << first_name << " " << second_name << " " << surname << endl; 
+         cout << "Gender: " << gender << endl; 
+         cout << "Date of Birth: " << date_of_birth << endl; 
+         cout << "Monthly Basic Salary: " << monthly_basic_salary << endl; 
+     } 
+     friend double compute_pension(Employee emp); 
+ }; 
+  
+ double compute_pension(Employee emp) { 
+     double pension = emp.monthly_basic_salary * 0.05; 
+     return pension; 
+ } 
+  
+ int main() { 
+     int id; 
+     string fn, sn, snn, dob; 
+     char g; 
+     double mbs; 
+      
+     cout << "Enter employee ID: "; 
+     cin >> id; 
+     cin.ignore(); // ignore the newline character 
+     cout << "Enter first name: "; 
+     getline(cin, fn); 
+     cout << "Enter second name: "; 
+     getline(cin, sn); 
+     cout << "Enter surname: "; 
+     getline(cin, snn); 
+     cout << "Enter gender (M/F): "; 
+     cin >> g; 
+     cout << "Enter date of birth (dd-mm-yyyy): "; 
+     cin >> dob; 
+     cout << "Enter monthly basic salary: \n"; 
+     cin >> mbs; 
+      
+     Employee emp_obj(id, fn, sn, snn, g, dob, mbs); 
+     emp_obj.show_employee(); 
+     cout << "Pension Contribution: " << compute_pension(emp_obj) << endl; 
+      
+     return 0; 
+ }
